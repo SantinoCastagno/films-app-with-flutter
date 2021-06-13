5e0cc34885fc69e11a353ddac9473d5733bc6d91
Santino Castagno <santinocastagno.dev@gmail.com>
2021-06-13 00:03:29 -0300

    Optimizations resources

--- lib/src/providers/peliculas_provider.dart
+++ lib/src/providers/peliculas_provider.dart
@@ -8,6 +8,7 @@ class PeliculasProvider {
   String _url = 'api.themoviedb.org';
   int _popularPage = 0;
   List<Pelicula> _populares = new List();
+  bool _cargando = false;
 
   //Creación del stream
   final _popularesStreamController =
@@ -27,12 +28,25 @@ class PeliculasProvider {
     _popularesStreamController.close();
   }
 
-  Future<List<Pelicula>> procesarRespuesta(Uri url) async {
-    final response = await http.get(url);
-    final decodedData = json.decode(response.body);
-    final Peliculas peliculas =
-        new Peliculas.fromJsonList(decodedData['results']);
-    return peliculas.items;
+  //Método que retorna de manera asincrónica una lista con las peliculas populares
+  getPopulares() async {
+    //Verificamos si se estan cargando otras peliculas para no hacer solicitudes innecesarias a la api.
+    if (!_cargando) {
+      _cargando = true;
+      _popularPage++;
+      final url = Uri.https(
+        _url,
+        '3/movie/popular',
+        {
+          'api_key': _apikey,
+          'page': _popularPage.toString(),
+        },
+      );
+      final _respuestaProcesada = await procesarRespuesta(url);
+      _populares.addAll(_respuestaProcesada);
+      popularesSink(_populares);
+      _cargando = false;
+    }
   }
 
   //Método que retorna de manera asincrónica una lista con las peliculas que se encuentran en cines.
@@ -47,20 +61,11 @@ class PeliculasProvider {
     return procesarRespuesta(url);
   }
 
-  //Método que retorna de manera asincrónica una lista con las peliculas populares
-  getPopulares() async {
-    _popularPage++;
-
-    final url = Uri.https(
-      _url,
-      '3/movie/popular',
-      {
-        'api_key': _apikey,
-        'page': _popularPage.toString(),
-      },
-    );
-    final _respuestaProcesada = await procesarRespuesta(url);
-    _populares.addAll(_respuestaProcesada);
-    popularesSink(_populares);
+  Future<List<Pelicula>> procesarRespuesta(Uri url) async {
+    final response = await http.get(url);
+    final decodedData = json.decode(response.body);
+    final Peliculas peliculas =
+        new Peliculas.fromJsonList(decodedData['results']);
+    return peliculas.items;
   }
 }
--- lib/src/widgets/movie_horizontal.dart
+++ lib/src/widgets/movie_horizontal.dart
@@ -23,10 +23,41 @@ class MovieHorizontal extends StatelessWidget {
 
     return Container(
       height: _screenSize.height * 0.3,
-      child: PageView(
+      child: PageView.builder(
         pageSnapping: false,
-        children: _tarjetas(context),
         controller: _pageController,
+        itemBuilder: (context, i) {
+          return _tarjeta(context, peliculas[i]);
+        },
+        itemCount: peliculas.length,
+      ),
+    );
+  }
+
+  Widget _tarjeta(BuildContext context, Pelicula pelicula) {
+    return Container(
+      margin: EdgeInsets.only(right: 15.0),
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: FadeInImage(
+              placeholder: AssetImage('assets/img/no-image.jpg'),
+              image: NetworkImage(pelicula.getPosterIMG()),
+              height: 120,
+              fit: BoxFit.fill,
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(
+            pelicula.title,
+            overflow: TextOverflow.ellipsis,
+            //manera directa de cambiar el estilo de la fuente:  style: TextStyle(fontSize: 12,),
+
+            style: Theme.of(context).textTheme.caption,
+            //utilizar un tema del contexto para cambiar el estilo de la fuente
+          ),
+        ],
       ),
     );
   }
